17b12a9e571610769314a1126fbbe610f8ecb6bd
Eric McCann <emccann@cs.uml.edu>
2014-01-01 14:23:13 -0500

    Intial commit of updater-script and the shell scripts that it runs.

---
+++ scripts/NuclearFallout.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# Eric McCann (Nuclearmistake) 2014
+
+always_clean() {
+cat <<EOF
+/system/app/Provision.apk
+/system/app/QuickSearchBox.apk
+/system/app/SetupWizard.apk
+/system/app/Vending.apk
+EOF
+}
+
+always_clean | while read FILE; do
+    rm -f $FILE
+done
+
+. /tmp/NuclearFalloutHelpers.sh
+
+# copy system files into place AND generate 70-gapps.sh while we do
+backuptool_header
+copy_files /tmp/system /system
+backuptool_footer
+
+# copy data files into place (if there are any)
+copy_files /tmp/data /data
